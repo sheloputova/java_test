24b4cfedc514c495a765a78a95b35db6cd6c672b
sheloputova <32092707+sheloputova@users.noreply.github.com>
2017-09-22 14:40:33 +0300

    еще 1 комит

--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -4,6 +4,10 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+       int l = 5;
+       int s = l * l;
+       System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 
 }
